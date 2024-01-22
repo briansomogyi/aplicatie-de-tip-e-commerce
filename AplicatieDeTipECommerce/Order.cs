6325053d4b7a7d480b22fc685edd2a61cc6a2db6
briansomogyi <brian.somogyi@emanuel.ro>
2024-01-22 09:12:41 +0200

    Implemented the Order class

---
+++ AplicatieDeTipECommerce/Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicatieDeTipECommerce
+{
+    public class Order
+    {
+        public Order(string customer, DateTime date, decimal total) 
+        {
+            Customer = customer;
+            Date = date;
+            Total = total;
+        }
+
+        public string Customer {  get; set; }
+
+        public DateTime Date { get; set; }
+
+        public decimal Total { get; set; }
+
+
+    }
+}
