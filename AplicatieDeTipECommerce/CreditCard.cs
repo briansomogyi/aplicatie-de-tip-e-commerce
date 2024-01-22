b805476a95f0f680791ac9df003b5913ad16ad54
briansomogyi <brian.somogyi@emanuel.ro>
2024-01-22 09:39:02 +0200

    Implemented the hieracy of derived classes from the abstract PaymentSystem class

---
+++ AplicatieDeTipECommerce/CreditCard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicatieDeTipECommerce
+{
+    internal class CreditCard:PaymentSystem
+    {
+        internal CreditCard(string paymentMethod) : base(paymentMethod) { }
+
+        internal override void PaymentFunctionality(decimal paymentAmount)
+        {
+            Console.WriteLine($"Paying ${paymentAmount} by {nameof(CreditCard)}.");
+        }
+    }
+}
